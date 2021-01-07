aa47ed1e6a21e6982e4eebeb7af431b91cb66384
Michael Blank <michaelblank14@gmail.com>
2021-01-07 15:40:01 -0500

    first commit

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -1,10 +1,3 @@
-//
-//  WeatherManager.swift
-//  Clima
-//
-//  Created by Angela Yu on 03/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
-//
 
 import Foundation
 import CoreLocation
